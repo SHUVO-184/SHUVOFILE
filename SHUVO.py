1cc70fc5cf642bc7e9612fd3963872c20cb66f0a
SHUVO-184 <abullbasher123456@gmail.com>
2024-09-17 12:40:06 +0800

    Update SHUVO.py

--- SHUVO.py
+++ SHUVO.py
@@ -1,9 +1,2 @@
 import ctypes
-
-# .so ফাইল লোড করা
 lib = ctypes.CDLL('./FILE_enc.cpython-311.so')
-
-# এখন আপনি সেখানে থাকা ফাংশনগুলো কল করতে পারবেন
-# উদাহরণ:
-result = lib.some_function()  # 'some_function' হল সেই ফাংশন যা .so ফাইলে আছে
-print(result)
